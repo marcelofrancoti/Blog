1a116ff1ba8d166b74f99c04f53eca7dfdc1275f
MARCELO FRANCO <marcelo_dkp@3coracoes.com.br>
2024-11-20 17:42:03 -0300

    aaaa

--- Blog.Teste/obj/Debug/net8.0/Blog.Teste.AssemblyInfo.cs
+++ Blog.Teste/obj/Debug/net8.0/Blog.Teste.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.Teste")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+564d3f370c838a40cf4c3887a02f664788118838")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.Teste")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.Teste")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
